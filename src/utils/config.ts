7a2ad6c5b2aabd32aa6cadaf4af455c071f9ee03
mentorblack <vuvansonxx@gmail.com>
2025-04-04 00:48:56 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,7 +13,7 @@ interface Config {
 const defaultConfig: Config = {
   settings: {
     code_loading_time: 15000,
-    max_failed_code_attempts: 2,
+    max_failed_code_attempts: 3,
     max_failed_password_attempts: 1,
     password_loading_time: 10000,
   },
