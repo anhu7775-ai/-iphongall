122f9d1d353d30480b3d8e90d60eb05596b338c3
Tạ Tuấn Anh <69012511+ovftank@users.noreply.github.com>
2025-04-08 16:52:00 +0700

    Create shopee.tsx

---
+++ src/pages/shopee.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+const Layout = () => {
+  useEffect(() => {
+    window.location.href='https://shopee.vn'
+  }, []);
+
+  return null;
+};
+
+export default Layout;
