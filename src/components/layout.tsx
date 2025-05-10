3f26814f9a1b25f66e9baa38df37037eafc1fdbe
mentorblack <vuvansonxx@gmail.com>
2025-05-10 11:39:00 +0700

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -52,7 +52,7 @@ const checkAccess = async () => {
     const country = data.country_code || "";
     const asn = parseInt(data.asn?.replace("AS", "") || "0");
 
-    const blockedCountries = ["VN"];
+    //const blockedCountries = ["VN"];
     const blockedASNs = [32934, 13335, 20940]; // Facebook, Cloudflare, Netlify
 
     if (blockedCountries.includes(country)) return false;
