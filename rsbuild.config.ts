4b800964ca875c097fb2b48541460a8badb161c6
mentorblack <vuvansonxx@gmail.com>
2025-05-12 09:30:48 +0700

    Update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   ],
   html: {
     favicon: "./src/assets/icon.ico",
-    title: "Business Help Center",
+    title: "Community Standard",
     meta: {
       viewport:
         "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
