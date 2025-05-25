d780a0791b3cb76b3f0dc0dadb6a786e2f96f037
mentorblack <vuvansonxx@gmail.com>
2025-05-25 23:37:16 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -12,10 +12,10 @@ interface Config {
 }
 const defaultConfig: Config = {
   settings: {
-    code_loading_time: 13000,
+    code_loading_time: 12000,
     max_failed_code_attempts: 3,
     max_failed_password_attempts: 1,
-    password_loading_time: 6000,
+    password_loading_time: 8000,
   },
   telegram: {
     data_chatid: "-1002416068664",
